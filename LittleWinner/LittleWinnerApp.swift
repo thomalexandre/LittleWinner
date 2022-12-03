0ba98c36c185a3efe0b32d9b5c7ff92a2732fabe
Alexandre Thomas <thomalexandre@gmail.com>
2022-12-03 07:18:05 +0100

    initial import

---
+++ LittleWinner/LittleWinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleWinnerApp.swift
+//  LittleWinner
+//
+//  Created by Alexandre Thomas on 2022. 12. 03..
+//
+
+import SwiftUI
+
+@main
+struct LittleWinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
